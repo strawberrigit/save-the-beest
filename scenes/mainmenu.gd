b394142e4cdc4844018364160caafd3ffade3ad8
Strawberry <zahraaabbasbha@gmail.com>
2026-07-15 14:30:50 +0300

    init

---
+++ scenes/mainmenu.gd
@@ -0,0 +1,9 @@
+extends Node
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level1.tscn")
+
+
+func _on_button_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level2.tscn")
